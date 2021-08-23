703a829b01d1bd9f8ba4fc23f92c807790f264e7
JJP_512 <jay.perez78@yahoo.com>
2021-08-23 18:00:54 -0500

    refactored app.js and finalized html

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const inquirer = require("inquirer");
 const { AnimationFrameScheduler } = require("rxjs/internal/scheduler/AnimationFrameScheduler");
-const fs = require("fs");
 const generatePage = require("./src/page-template.js");
+const {writeFile, copyFile} = require("./utils/generate-site.js");
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -135,11 +136,18 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, err => {
-        if (err) throw err;
-
-        console.log("Portfolio complete! Check out index.html to see the output!");
-    });
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
